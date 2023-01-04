27a0bd3bd5b767a0e16bffb69b0bd093c3b79d2d
Taylor Hughes <taylor@taylor-hughes.com>
2023-01-03 17:18:11 -0800

    Handle finish login flow more gracefully

--- app/routes/login/$slug.tsx
+++ app/routes/login/$slug.tsx
@@ -15,56 +15,71 @@ type LoaderData = {
     slug: string;
 };
 
+function persistCredentials(slug: string, window: BrowserWindow) {
+    // Logged in! Get the cookies from appropriate domains
+    const cookiePromises = [];
+    cookiePromises.push(
+        window.webContents.session.cookies.get({
+            url: "https://app.slack.com/",
+        }),
+    );
+    cookiePromises.push(
+        window.webContents.session.cookies.get({
+            url: "https://edgeapi.slack.com/",
+        }),
+    );
+
+    const duplicates = new Set<string>();
+    const cookies = new Array<Cookie>();
+    Promise.all(cookiePromises).then((cookiesFromDifferentUrls) => {
+        cookiesFromDifferentUrls.forEach((cookieList) => {
+            cookieList.forEach((cookie) => {
+                const cookieJson = JSON.stringify(cookie);
+                if (duplicates.has(cookieJson)) {
+                    return;
+                }
+                duplicates.add(cookieJson);
+                cookies.push(cookie);
+            });
+        });
+
+        storeCredentials(slug, cookies);
+        clearLoadingState(slug);
+    });
+}
+
 export function loader({
     params: { slug },
 }: {
     params: { slug: string };
 }): TypedResponse<LoaderData> {
+    const destinationUrl = `https://${slug}.slack.com/unreads`;
+
     if (!slackWindow) {
-        const slackSession = session.fromPartition(`persist:slack:${slug}`);
+        const slackSession = session.fromPartition(`persist:slack`);
+        slackSession.protocol.registerFileProtocol(
+            "slack",
+            (request, callback) => {
+                callback(""); // errors out the attempt to open the slack app
+                setTimeout(() => {
+                    slackWindow!.loadURL(destinationUrl);
+                }, 0);
+            },
+        );
+
         slackWindow = new BrowserWindow({
             width: 1024,
             height: 768,
             webPreferences: { session: slackSession },
         });
 
-        slackWindow.loadURL(`https://${slug}.slack.com/unreads`);
+        slackWindow.loadURL(destinationUrl);
     } else if (
         slackWindow.webContents
             .getURL()
             .startsWith("https://app.slack.com/client")
     ) {
-        // Logged in! Get the cookies from appropriate domains
-        const cookiePromises = [];
-        cookiePromises.push(
-            slackWindow.webContents.session.cookies.get({
-                url: "https://app.slack.com/",
-            }),
-        );
-        cookiePromises.push(
-            slackWindow.webContents.session.cookies.get({
-                url: "https://edgeapi.slack.com/",
-            }),
-        );
-
-        const duplicates = new Set<string>();
-        const cookies = new Array<Cookie>();
-        Promise.all(cookiePromises).then((cookiesFromDifferentUrls) => {
-            cookiesFromDifferentUrls.forEach((cookieList) => {
-                cookieList.forEach((cookie) => {
-                    const cookieJson = JSON.stringify(cookie);
-                    if (duplicates.has(cookieJson)) {
-                        return;
-                    }
-                    duplicates.add(cookieJson);
-                    cookies.push(cookie);
-                });
-            });
-
-            storeCredentials(slug, cookies);
-            clearLoadingState(slug);
-        });
-
+        persistCredentials(slug, slackWindow);
         slackWindow.close();
         slackWindow = null;
         return redirect(`/unread/${slug}`);
